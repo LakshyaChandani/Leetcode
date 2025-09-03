370be5eef86b3978e0946d7752775e4e00e03e1e
Lakshya <173071066+LakshyaChandani@users.noreply.github.com>
2025-09-03 06:05:42 +0000

    Sync LeetCode submission Runtime - 10 ms (6.97%), Memory - 8.6 MB (37.45%)

--- submissions/0009-palindrome-number/solution.cpp
+++ submissions/0009-palindrome-number/solution.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return false;
-        long long int rNum = 0;
-        long long int num = x;
+        long long int rNum = 0, num = x;
         while(x>0){
             rNum = (rNum*10) + x%10;
             x/=10; 
         }
         if(num == rNum) return true;
-        return false;
+        else{return false;}
     }
 };
