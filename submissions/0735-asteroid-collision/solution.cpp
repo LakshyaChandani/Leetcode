20988dc2ab37069f9b943eaa8ab46d3dbd1cc875
Lakshya <173071066+LakshyaChandani@users.noreply.github.com>
2026-07-05 13:01:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.7 MB (71.71%)

--- submissions/0735-asteroid-collision/solution.cpp
+++ submissions/0735-asteroid-collision/solution.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int, vector<int>> st;
-        for(int asteroid:asteroids){
-            if(st.empty()) st.push(asteroid);
-            else if (st.top()>0 && asteroid>0) st.push(asteroid);
-            else if (st.top()<0) st.push(asteroid);
+        vector<int> st;
+        for(int asteroid: asteroids){
+            if (asteroid>0) st.push_back(asteroid);
             else{
-                if(st.top() > abs(asteroid)) continue;
-                else if(st.top() == abs(asteroid)) st.pop();
-                else{
-                    while(!st.empty() && st.top()>0 && st.top()<abs(asteroid)) st.pop();
-                    if(st.empty()) st.push(asteroid);
-                    else if(st.top()<0) st.push(asteroid);
-                    else if(st.top() == abs(asteroid)) st.pop();
-                    else continue;
-                }
-                
-            }
+                while(!st.empty() && st.back()>0 && st.back()<abs(asteroid)) st.pop_back();
+                if(st.empty() || st.back() < 0) st.push_back(asteroid);
+                if(st.back() == abs(asteroid)) st.pop_back();
+            }   
         }
-
-        int k = st.size() - 1;
-        vector<int> remaining(k+1);
-
-        while(!st.empty()){
-            remaining[k] = st.top();
-            st.pop();
-            k--;
-        }
-        
-        return remaining;
-
+        return st;
     }
 };
