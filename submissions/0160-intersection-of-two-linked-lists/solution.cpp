02af97ef37c0407d292ce880d87844b337563705
Lakshya <173071066+LakshyaChandani@users.noreply.github.com>
2026-07-22 17:59:38 +0000

    Sync LeetCode submission Runtime - 54 ms (71.35%), Memory - 23.9 MB (86.83%)

--- submissions/0160-intersection-of-two-linked-lists/solution.cpp
+++ submissions/0160-intersection-of-two-linked-lists/solution.cpp
@@ -9,47 +9,22 @@
 class Solution {
 public:
 
-    ListNode* collisionPoint(ListNode* larger, ListNode* smaller, int difference){
-
-        while(difference){
-            larger = larger->next;
-            difference--;
-        }
-
-        while(smaller && larger){
-            if(smaller == larger) return smaller;
-            smaller = smaller->next;
-            larger = larger->next;
-        }
-        
-        return NULL;
-    }
-
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-
-        int countA = 0;
-        int countB = 0;
 
         ListNode* tempA = headA;
         ListNode* tempB = headB;
 
-        while(tempA){
-            countA++;
-            tempA = tempA->next;
+        while(tempA != tempB){
+
+            if(!tempA) tempA = headB;
+            else tempA = tempA->next;
+
+            if(!tempB) tempB = headA;
+            else tempB = tempB->next;
+
         }
 
-        while(tempB){
-            countB++;
-            tempB = tempB->next;
-        }
-
-
-        if(countA>countB){
-            return collisionPoint(headA,headB,countA-countB);
-        }
-        else{
-            return collisionPoint(headB,headA,countB-countA);
-        }
-        
+        return tempA;
     }
+
 };
