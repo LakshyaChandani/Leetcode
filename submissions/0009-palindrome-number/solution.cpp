82c92e142b93cb236473e60ff20eb190b69b35be
Lakshya <173071066+LakshyaChandani@users.noreply.github.com>
2026-07-24 17:29:09 +0000

    Sync LeetCode submission Runtime - 2 ms (50.70%), Memory - 8.6 MB (37.71%)

--- submissions/0009-palindrome-number/solution.cpp
+++ submissions/0009-palindrome-number/solution.cpp
@@ -1,19 +1,13 @@
-#include <iostream>
-#include <string>
-using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string num = to_string(x);
-        int left = 0;
-        int right = num.size() - 1;
-        while(right>left){
-            if (num[left] != num[right]){
-                return false;
-            }
-            left++;
-            right--;
+        if(x<0) return false;
+        long long int rNum = 0, num = x;
+        while(x>0){
+            rNum = (rNum*10) + x%10;
+            x/=10; 
         }
-        return true;
+        if(num == rNum) return true;
+        return false;
     }
 };
