e229d67021fa046bca88bc0f967b5d4792c06ebc
Lakshya <173071066+LakshyaChandani@users.noreply.github.com>
2026-07-26 20:28:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (93.01%)

---
+++ submissions/0058-length-of-last-word/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for(int i = s.size()-1;i>=0;i--){
+            if(s[i] != ' '){
+                while(i>=0 && s[i] !=' '){
+                    count++;
+                    i--;
+                }
+                return count;
+            }
+        }
+        return -1;
+    }
+};
